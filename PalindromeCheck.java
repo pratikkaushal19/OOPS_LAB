ca7d1f8c4f087628a1b9c05c4020ce8224f3f0b9
pratikkaushal19 <146801540+pratikkaushal19@users.noreply.github.com>
2025-02-12 16:29:18 +0530

    Add files via upload

---
+++ PalindromeCheck.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        scanner.close();
+        
+        if (isPalindrome(str)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+    
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
